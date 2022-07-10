019c3ab9ea0bc51988bf8e4e28fa556e00bd5f43
David Freitas <davidfreitas@MacBook-Pro-de-David.local>
2022-07-10 12:54:06 -0300

    configuracao do projeto

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className="bg-violet-500 p-2">{props.text ?? 'Default'}</button>
+}
+
+function App() {
+  return (
+    <div>
+      <Button text="Enviar" />
+      <Button text="ok" />
+      <Button />
+    </div>
+  )
+}
+
+export default App
